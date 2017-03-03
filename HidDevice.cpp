eb5b4eb975bfcb5bf797fd4cda16336b7624726d
Adriana Reus <adriana.reus@nxp.com>
2017-03-03 12:15:15 +0200

    Add USB VID and PID for iMX8QM
    Signed-off-by: Adriana Reus <adriana.reus@nxp.com>

--- HidDevice.cpp
+++ HidDevice.cpp
@@ -216,6 +216,9 @@ else INVLAID_HANDLE_VALUE.
 		case K32H422:
 			filter.Format(_T("%s#vid_%04x&pid_%04x"), _T("HID"), FSL_VID, K32H422_USB_PID);
 			break;
+		case MX8QM:
+			filter.Format(_T("%s#vid_%04x&pid_%04x"), _T("HID"), NXP_VID, MX8QM_USB_PID);
+			break;
 		default:
 			filter.Format(_T("%s#vid_%04x&pid_%04x"), _T("HID"), 0xFFFF, 0xFFFF);
 			break;
